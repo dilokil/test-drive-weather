acfaf7f0701b47b89decc3208081bb3ed9f4db3a
Alexandr Rubtsov <AlRubtsov@croc.ru>
2023-04-16 19:39:00 +0300

    правки

--- src/main/java/ru/croc/weather/service/QuoteService.java
+++ src/main/java/ru/croc/weather/service/QuoteService.java
@@ -3,11 +3,20 @@ package ru.croc.weather.service;
 import org.springframework.stereotype.Service;
 import ru.croc.weather.model.Quote;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class QuoteService {
 
+    private final List<Quote> quotes = new ArrayList<>();
+
+    public QuoteService() {
+        quotes.add(new Quote("Цитита", "автор"));
+    }
+
     public Quote getRandomQuote() {
 
-        return new Quote();
+        return quotes.get(0);
     }
 }
